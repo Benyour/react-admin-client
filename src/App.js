18600c29b21f1494f3640b470f4885ab6853e771
alibaba_zhangyabin <zhangyabin@git.shhlee.com>
2020-08-02 19:40:18 +0800

    引入react-router-dom并搭建基础路由环境

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react'
-import { Button } from 'antd';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import 'antd/dist/antd.css'
+
+import Login from './pages/Login'
+import Admin from './pages/Admin'
 export default class App extends Component {
     render () {
         return (
-            <div className="App">
-                <Button type="primary">Button</Button>
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/" component={Admin}></Route>
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
