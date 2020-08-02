ad39cbecbd1b485d7f9d156ac608f0bb19281218
alibaba_zhangyabin <zhangyabin@git.shhlee.com>
2020-08-02 19:21:32 +0800

    集成阿里antDesign库

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react'
-
+import { Button } from 'antd';
+import 'antd/dist/antd.css'
 export default class App extends Component {
     render () {
-        return <div>Hello, React Admin</div>
+        return (
+            <div className="App">
+                <Button type="primary">Button</Button>
+            </div>
+        )
     }
 }
